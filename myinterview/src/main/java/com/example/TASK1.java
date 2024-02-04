c14bef4ffd9c08b5535e1ac8a0d81772f10adaf1
CesarOli <cesaraleoliveira@gmail.com>
2024-02-04 01:57:43 -0300

    TASK 1.

--- myinterview/src/main/java/com/example/TASK1.java
+++ myinterview/src/main/java/com/example/TASK1.java
@@ -13,6 +13,32 @@ import java.util.Scanner;
  * racecar.
  */
 public class TASK1 {
+     public static class VerificaFrasePalindromo {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Diga uma frase qualquer para detectar se é ou não um PALÍNDROMO: ");
+            String input = scanner.nextLine().trim().toUpperCase();
+
+            String[] palavras = input.split("\\s+");
+            String juntar = String.join("", palavras);
+
+            StringBuilder inverter = new StringBuilder();
+            for(int i = juntar.length() -1; i>=0; i--) {
+                inverter.append(juntar).charAt(i);
+            }
+
+            if(inverter.toString().equals(juntar)) {
+                System.out.println(("A frase " + scanner + " é Palíndromo."));
+            }
+            else {
+                System.out.println("Esta frase " + scanner + " não é um Palíndromo.");
+            }
+
+
+        }
      
+        
+     }
  
-}
+} 
