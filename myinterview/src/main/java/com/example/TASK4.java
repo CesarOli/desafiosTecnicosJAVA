ed2ed7977195a895568632d510808ea228ef2f63
CesarOli <cesaraleoliveira@gmail.com>
2024-02-11 01:40:22 -0300

    Configuração do ambiente de desenvolvimento realizado com sucesso.

--- myinterview/src/main/java/com/example/TASK4.java
+++ myinterview/src/main/java/com/example/TASK4.java
@@ -8,5 +8,7 @@ package com.example;
  *
  */
 public class TASK4 {
-
+    public static void main(String[] args) {
+        System.out.println("Ambiente de Desenvolvimento JAVA configurado com sucesso!!");
+    }
 }
