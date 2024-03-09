0583bdedacba2a751a86d15b38b5670a53c0a4e0
CesarOli <cesaraleoliveira@gmail.com>
2024-03-08 23:53:04 -0300

    Retirada de comentários extras.

--- myinterview/src/main/java/com/example/TASK4.java
+++ myinterview/src/main/java/com/example/TASK4.java
@@ -28,48 +28,49 @@ import java.util.Map;;
 public class TASK4 {
     public static void main(String[] args) {
 
-        //criação do cliente HTTP
         HttpClient httpClient = HttpClients.createDefault();
-
-        //definição do endpoint da API
         String apiUrl = "https://3ospphrepc.execute-api.us-west-2.amazonaws.com/prod/RDSLambda";
-
-        //criação de uma solicitação GET
         HttpGet httpGet = new HttpGet(apiUrl);
 
         try {
-            //executa a soliticação e obtem a resposta
             HttpResponse response = httpClient.execute(httpGet);
-            
-            // obtem o corpo da resposta como uma string
             String responseBody = EntityUtils.toString(response.getEntity());
 
-            //processa a resposta(analisa, exibe, salva)
             System.out.println("Código de status da resposta: " + response.getStatusLine().getStatusCode());
             System.out.println("Corpo da resposta: " + responseBody);
 
-            //adição da lógica para analisar o corpo da resposta JSON
             ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode jsonNode = objectMapper.readTree(responseBody);
+            JsonNode jsonResponse = objectMapper.readTree(responseBody);
 
-            //inicializa o mapa para armazenar a contagem de registro por gênero
-            Map<String, Integer> genderCountMap = new HashMap<>();
+            if (jsonResponse.has("data") && jsonResponse.get("data").isArray()) {
+                JsonNode dataNode = jsonResponse.get("data");
 
-            //iteração sobre os registros no JSON
-            for (JsonNode record : jsonNode) {
+                Map<String, Integer> genderCountMap = new HashMap<>();
 
-                //obtem valor do campo "gender"
-                String gender = record.get("gender").asText();
+                for (JsonNode record : dataNode) {
+                    JsonNode genderNode = record.get("gender");
 
-                //atualiza a contagem no mapa
-                genderCountMap.put(gender, genderCountMap.getOrDefault(gender, 0) + 1);
+                    if (genderNode != null && !genderNode.isNull()) {
+                        String gender = genderNode.asText();
+                        genderCountMap.put(gender, genderCountMap.getOrDefault(gender, 0) + 1);
+                    }
+                }
+
+                //Exibir o conteudo do Json para depurar
+                System.out.println("Conteúdo do JSON");
+                System.out.println(responseBody);
+
             }
+                //Exibição do resultado
+                System.out.println("Contagem de registro por gênero:");
+                for (Map.Entry<String, Integer> entry : genderCountMap.entrySet()) {
+                    System.out.println(entry.getKey() + ": " + entry.getValue());
+                }
 
-            //Exibição do resultado
-            System.out.println("Contagem de registro por gênero:");
-            for (Map.Entry<String, Integer> entry : genderCountMap.entrySet()) {
-                System.out.println(entry.getKey() + ": " + entry.getValue());
-            }
+            //Exibir o conteudo do Json para depurar
+            System.out.println("Conteúdo do JSON");
+            System.out.println(responseBody);
+
 
         } catch (IOException e) {
             // tratamento da exceção IOException
